1faa46d84080482eeecf2cda06fab4f851a0d66d
Caldis <532607446@qq.com>
2014-09-15 16:13:09 +0800

    Initial Commit

---
+++ MControl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MControl
+//
+//  Created by Cyrus on 14-9-15.
+//  Copyright (c) 2014年 Cb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MControl/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MControl
+//
+//  Created by Cyrus on 14-9-15.
+//  Copyright (c) 2014年 Cb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
